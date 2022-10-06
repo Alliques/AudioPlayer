32cf761e620907695e9735922c0a70361c3b730b
Alliques <airat1100@bk.ru>
2022-10-06 17:37:52 +0300

    tracklist smooth scrolling

---
+++ Walkman.UI/Behavors/SmoothScrollViewerBehavior.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Reflection;
+using System.Windows.Controls.Primitives;
+using Walkman.UI.Controls.CustomControls;
+
+namespace Walkman.UI.Behavors
+{
+    public class SmoothScrollViewerBehavior : Behavior<CustomScrollViewer>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Loaded += ScrollViewerLoaded;
+        }
+
+        private void ScrollViewerLoaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var property = AssociatedObject.GetType().GetProperty("ScrollInfo", BindingFlags.NonPublic | BindingFlags.Instance);
+            property.SetValue(AssociatedObject, new ScrollInfoAdapter((IScrollInfo)property.GetValue(AssociatedObject)));
+        }
+    }
+}
---
+++ Walkman.UI/Controls/CustomControls/CustomScrollViewer.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Walkman.UI.Controls.CustomControls
+{
+    public class CustomScrollViewer : ScrollViewer
+    {
+        public CustomScrollViewer()
+        {
+            Loaded += ScrollViewer_Loaded;
+        }
+
+        private void ScrollViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            ScrollInfo = new ScrollInfoAdapter(ScrollInfo);
+        }
+    }
+}
