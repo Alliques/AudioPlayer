a837e29b4a6e3da5e0b03ef0948db38563074c34
Alliques <airat1100@bk.ru>
2022-10-21 22:44:57 +0300

    changed the main layout container

---
+++ Walkman.UI/Converter/InLessThanConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Walkman.UI.Converter
+{
+    /// <summary>
+    /// InLessThanConverter
+    /// </summary>
+    public class InLessThanConverter : IValueConverter
+    {
+        public static readonly InLessThanConverter Instance = new InLessThanConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo cultureInfo)
+        {
+            double doubleValue = System.Convert.ToDouble(value);
+            double compareToValue = System.Convert.ToDouble(parameter);
+
+            return doubleValue < compareToValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// InGreaterThanConverter
+    /// </summary>
+    public class InGreaterThanConverter : IValueConverter
+    {
+        public static readonly InGreaterThanConverter Instance = new InGreaterThanConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo cultureInfo)
+        {
+            double doubleValue = System.Convert.ToDouble(value);
+            double compareToValue = System.Convert.ToDouble(parameter);
+
+            return doubleValue < compareToValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Walkman.UI/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Walkman.UI.ViewModels.Base
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action mAction;
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        #region Constructor
+        public RelayCommand(Action action)
+        {
+            mAction = action;
+        }
+        #endregion
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            mAction();
+        }
+    }
+}
